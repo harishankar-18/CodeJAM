752837ffea7aa4008a0ebd69c3e9a448682d4ace
harishankar-18 <61908805+harishankar-18@users.noreply.github.com>
2020-06-04 20:41:32 +0530

    Update and rename attempt1.py to attempt.py

--- CodeJam_2/attempt.py
+++ CodeJam_2/attempt.py
@@ -1,5 +1,17 @@
 import cv2
 import numpy as np
+
+im1 = cv2.imread('1.jpg',0)
+ret1,thresh1 = cv.threshold(im1, 127, 255,0)
+
+im2 = cv2.imread('2.jpg',0)
+ret2,thresh2 = cv.threshold(im2, 127, 255,0)
+#contours of images of sample images of 1 and 2
+contours,hierarchy = cv2.findContours(thresh1, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+c1 = contours[0]
+contours,hierarchy = cv2.findContours(thresh2, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+c2 = contours[0]
+
 cap = cv2.VideoCapture('sentry3.mkv')
 while cap.isOpened():
     ret1,frame = cap.read()
@@ -20,7 +32,16 @@ while cap.isOpened():
         try:
             for c in (cnt[0],cnt[1]):
                 (x,y,w,h) = cv2.boundingRect(c)
-                cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+                #cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+                r1 = cv.matchShapes(cnt,c1,1,0.0)
+                r2 = cv.matchShapes(cnt,c2,1,0.0)
+                if (r1>r2):     #contour is more similar to contour of image of 1
+                    cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+                elif(r2>r1):
+                    cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),2)
+
+                    
+               
         except:
             pass
         cv2.imshow('frame',frame)
